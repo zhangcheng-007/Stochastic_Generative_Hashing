bdaab84812771a0eaf6b90755e563d519d84ee8c
zhangcheng-007 <54843450+zhangcheng-007@users.noreply.github.com>
2020-02-25 13:17:44 +0800

    Update stochastic_hashing.py

--- sgh_notebook/stochastic_hashing.py
+++ sgh_notebook/stochastic_hashing.py
@@ -112,7 +112,7 @@ if __name__ == "__main__":
     # algorithm parameters
     dim_input = 28 * 28
     # length of bits
-    dim_hidden= int(sys.argv[1]) 
+    dim_hidden= int(sys.argv[1]-1) 
     print('dim of hidden variable is %d' %(dim_hidden))
 
     batch_size = 500
